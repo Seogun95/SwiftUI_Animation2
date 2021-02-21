a7394fc6d8e737046acb0a2c5298813dc7b3bc59
SeonJungKim <ksj083895@gmail.com>
2021-02-21 17:41:40 +0500

    Initial Commit

---
+++ SwiftUI_Animation2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Animation2
+//
+//  Created by 김선중 on 2021/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Animation2/SwiftUI_Animation2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation2App.swift
+//  SwiftUI_Animation2
+//
+//  Created by 김선중 on 2021/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
